46a4e5af8427158fe7f37ed5efd067bedbdec0b8
Jaime Solís Soto <A00759756@itesm.mx>
2017-10-26 13:14:06 -0600

    trackbar

--- TrackBarA00759756/Form1.Designer.cs
+++ TrackBarA00759756/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(239, 81);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(252, 171);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 453);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Form1_Scroll);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TrackBarA00759756/Form1.cs
+++ TrackBarA00759756/Form1.cs
@@ -15,6 +15,29 @@ namespace TrackBarA00759756
         public Form1()
         {
             InitializeComponent();
+            
+            //label1.Text = "warning";
+            TrackBar trackBar1 = new TrackBar();
+            TrackBar trackBar2 = new TrackBar();
+
+            trackBar1.Maximum = 10;
+            trackBar2.Maximum = 10;
+        }
+
+
+
+        private void Form1_Scroll(object sender, ScrollEventArgs e)
+        {
+           
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            if ((trackBar1.Value > 5) & (trackBar2.Value > 5))
+            {
+                label1.Text = "warning";
+            }
         }
     }
 }
